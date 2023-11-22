9095871bf141f772c117dab2c2756e81f76b08b6
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2023-11-22 14:14:27 +0100

    Remove other languages than Java

--- js/tests/safe-calculotor.test.js
+++
@@ -1,7 +0,0 @@
-const test = require('tape')
-
-test('should not throw when authorized', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // SafeCalculator.add()
-  t.end()
-})
--- ts/tests/safe-calculotor.test.ts
+++
@@ -1,7 +0,0 @@
-import test from "tape"
-
-test('should not throw when authorized', (t) => {
-  // TODO: write a test that fails due to the bug in
-  // SafeCalculator.add()
-  t.end()
-})
