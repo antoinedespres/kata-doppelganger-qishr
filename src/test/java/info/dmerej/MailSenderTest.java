197c11743a7ea6cec2f9dfd963b4c41ee41f36d7
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2023-11-22 15:38:46 +0100

    Mock for MailSenderTest v1
    Pilot: Luka LUPATADZE <luka.kupatadze@efrei.net>

--- src/test/java/info/dmerej/MailSenderTest.java
+++ src/test/java/info/dmerej/MailSenderTest.java
@@ -1,11 +1,44 @@
 package info.dmerej;
 
+import info.dmerej.mailprovider.SendMailRequest;
+import info.dmerej.mailprovider.SendMailResponse;
+import jdk.jfr.Frequency;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MockHttpClient implements HttpClient{
+    int code;
+    private Object sendMailRequest;
+
+    public MockHttpClient(int code) {
+        this.code = code;
+    }
+
+    public SendMailResponse post(String url, Object request) {
+        sendMailRequest = request;
+        return new SendMailResponse(this.code, "OK");
+    }
+
+    public Object getSendMailRequest(){
+        return sendMailRequest;
+    }
+}
+
+
 public class MailSenderTest {
+
     @Test
     void should_make_a_valid_http_request() {
-        // TODO: write a test to demonstrate the bug in MailSender.sendV1()
+
+        User user = new User("michel", "michel@efrei.net");
+        MockHttpClient mockHttpClient = new MockHttpClient(200);
+        MailSender mailSender = new MailSender(mockHttpClient);
+        SendMailRequest sendMailRequest = new SendMailRequest("michel@efrei.net","New notification","message1");
+
+        mailSender.sendV1(user,"message1");
+
+        assertEquals(sendMailRequest, mockHttpClient.getSendMailRequest());
     }
 
     @Test
