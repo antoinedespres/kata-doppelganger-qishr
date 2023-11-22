1858dc9ddba8c79e539373f8839f3b153d146acb
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2023-11-22 15:59:34 +0100

    Mockito SafeCalculator (pilot: Hugo J)

--- src/test/java/info/dmerej/SafeCalculatorTest.java
+++ src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -3,6 +3,8 @@ package info.dmerej;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class DummyAuthorizer implements Authorizer {
   @Override
@@ -13,10 +15,20 @@ class DummyAuthorizer implements Authorizer {
 
 
 public class SafeCalculatorTest {
-
+  
   @Test
   void should_not_throw_when_authorized() {
     Authorizer authorizer = new DummyAuthorizer();
+    SafeCalculator calculator = new SafeCalculator(authorizer);
+
+    assertEquals(3, calculator.add(1, 2));
+  }
+
+  @Test
+  void should_not_throw_when_authorized_mockito() {
+    Authorizer authorizer = mock();
+    when(authorizer.authorize()).thenReturn(true);
+
     SafeCalculator calculator = new SafeCalculator(authorizer);
 
     assertEquals(3, calculator.add(1, 2));
