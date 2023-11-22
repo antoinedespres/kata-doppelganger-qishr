075028e7c346e6e312241f70f03183119e9901b9
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2023-11-22 16:12:49 +0100

    Mockito DiscountApplier v1 Pilot: zain zafar

--- src/test/java/info/dmerej/DiscountApplierTest.java
+++ src/test/java/info/dmerej/DiscountApplierTest.java
@@ -1,11 +1,14 @@
 package info.dmerej;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 record Notification(User user, String message) {}
 
@@ -50,6 +53,23 @@ public class DiscountApplierTest {
     discountApplier.applyV2(10, users);
 
     assertEquals(users, notifier.getNotifications().stream().map(Notification::user).toList());
+  }
+
+  @Test
+  void should_notify_twice_when_applying_discount_for_two_users_v1_mockito() {
+    Notifier notifier = Mockito.mock(Notifier.class);
+    DiscountApplier discountApplier = new DiscountApplier(notifier);
+    User user1 = new User("michel", "michel@efrei.net");
+    User user2 = new User("bastien", "bastien@efrei.net");
+
+    discountApplier.applyV1(10, List.of(user1, user2));
+
+    Mockito.verify(notifier, Mockito.times(2)).notify(Mockito.any(), Mockito.any());
+  }
+
+  @Test
+  void should_notify_twice_when_applying_discount_for_two_users_v2_mockito() {
+
   }
 
 }
